bf55fb55657bb9e599a35566c8eaaa1760215f90
mpietrewicz <mpietrewicz@gmail.com>
2022-07-21 11:08:42 +0200

    Inicjacja projektu

---
+++ src/test/java/pl/altkom/clouddemo/CloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.clouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
